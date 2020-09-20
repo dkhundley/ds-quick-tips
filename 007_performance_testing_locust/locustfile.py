a069e3387d68e43416be7a2b7626dc8d102a5079
David Hundley <david.k.hundley@gmail.com>
2020-09-20 17:59:26 -0500

    Adding Locustfile comments

--- 007_performance_testing_locust/locustfile.py
+++ 007_performance_testing_locust/locustfile.py
@@ -1,13 +1,17 @@
 from locust import HttpUser, task, between
 import json
 
+# Loading the test JSON data
 with open('test_data/test_1.json') as f:
     test_data = json.loads(f.read())
 
+# Creating an API User class inheriting from Locust's HttpUser class
 class APIUser(HttpUser):
+    # Setting the host name and wait_time
     host = 'http://localhost:5001'
     wait_time = between(3, 5)
 
+    # Defining the post task using the JSON test data
     @task()
     def predict_endpoint(self):
         self.client.post('/predict', json = test_data)
