b63ae0bcc2e51d18903283f6f809f99fbf376a7d
David Hundley <david.k.hundley@gmail.com>
2020-10-25 13:38:03 -0500

    Adding new Flask vs FastAPI entry

---
+++ 011_fastapi_vs_flask/flask_api.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import joblib
+from flask import Flask, request, json, Response, jsonify, make_response
+
+## PRELOADED COMPONENTS
+# ------------------------------------------------------------------------------
+# Instantiating the Flask application
+application = Flask(__name__)
+
+# Loading the saved, serialized model
+model = joblib.load('../model/rfc_pipeline.pkl')
+
+
+
+## API ENDPOINTS
+# ------------------------------------------------------------------------------
+# Defining our prediction endpoint
+@application.route('/predict', methods = ['POST'])
+def predict():
+
+    # Getting incoming data from request
+    predict_json = request.json
+
+    # Transforming JSON data to DataFrame
+    predict_df = pd.json_normalize(predict_json)
+
+    # Running data through model
+    preds = model.predict(predict_df)
+
+    # Prepping preds to be returned to user
+    js = json.dumps({'preds': str(preds[0])})
+
+    return Response(js, status = 200, mimetype = 'application/json')
+
+
+
+# Defining a basic health endpoint
+@application.route('/health', methods = ['GET'])
+def health():
+
+    # Dumping out simple health message
+    js = json.dumps({'Status': 'Healthy!'})
+
+    return Response(js, status = 200, mimetype = 'application/json')
